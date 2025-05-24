acf299c997ec3a4324fd2004f3b48f1fdc1650a7
ecvsgl <efecav95@gmail.com>
2025-05-25 00:00:01 +0300

    StoreRepository added

---
+++ src/main/java/com/efecavusoglu/couriertracking/repository/StoreRepository.java
@@ -0,0 +1,12 @@
+package com.efecavusoglu.couriertracking.repository;
+
+import com.efecavusoglu.couriertracking.model.entity.StoreEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+    Optional<StoreEntity> findByStoreName(String name);
+}
